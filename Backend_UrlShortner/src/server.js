cecf7d618725badc9e429d30857dcc7e5118cc87
Aakriti Agrahari <agrahariaakriti1@gmail.com>
2026-02-11 20:08:40 +0530

    Simplify CORS configuration by using default settings

--- Backend_UrlShortner/src/server.js
+++ Backend_UrlShortner/src/server.js
@@ -4,17 +4,7 @@ import express from "express";
 import urlRoute from "./routes/url.route.js";
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "https://gilded-custard-e12c11.netlify.app",
-      "http://localhost:5173",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
