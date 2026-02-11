5fe737b2791980af595eccc9bcd66551907cd960
Aakriti Agrahari <agrahariaakriti1@gmail.com>
2026-02-11 20:45:34 +0530

    Refactor CORS configuration to explicitly define settings for allowed origins and methods

--- Backend_UrlShortner/src/server.js
+++ Backend_UrlShortner/src/server.js
@@ -4,7 +4,19 @@ import express from "express";
 import urlRoute from "./routes/url.route.js";
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://gilded-custard-e12c11.netlify.app",
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
