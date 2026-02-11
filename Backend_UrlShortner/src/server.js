ec892f2940cdf3c002f9427cda144e532f1e338c
Aakriti Agrahari <agrahariaakriti1@gmail.com>
2026-02-11 20:03:32 +0530

    Fix CORS configuration by removing trailing slash from production URL

--- Backend_UrlShortner/src/server.js
+++ Backend_UrlShortner/src/server.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://gilded-custard-e12c11.netlify.app/",
-      // "http://localhost:5173",
+      "https://gilded-custard-e12c11.netlify.app",
+      "http://localhost:5173",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
